8a68a2abf4afad8f27ba4df3884e578e263522bb
Venkata krishnan Sowrirajan <vsowrirajan@linkedin.com>
2021-02-23 22:20:56 -0800

    new changes

--- src/main/scala/com/qubole/parser/TFileInputFormat.scala
+++ src/main/scala/com/qubole/parser/TFileInputFormat.scala
@@ -17,63 +17,14 @@
  */
 package com.qubole.parser
 
-import org.apache.hadoop.conf.{Configurable => TConfigurable, Configuration}
 import org.apache.hadoop.fs.Path
 import org.apache.hadoop.io.Text
-import org.apache.hadoop.mapreduce.lib.input.{CombineFileInputFormat, CombineFileRecordReader, CombineFileSplit}
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat
 import org.apache.hadoop.mapreduce.{InputSplit, JobContext, RecordReader, TaskAttemptContext}
 
-import scala.collection.JavaConverters._
 
-class TFileInputFormat extends CombineFileInputFormat[Text, Text] with Configurable {
+class TFileInputFormat extends FileInputFormat[Text, Text] {
+  def createRecordReader(split: InputSplit, context: TaskAttemptContext): RecordReader[Text, Text] = new TFileRecordReader
 
-  override protected def isSplitable(context: JobContext, file: Path): Boolean = false
-
-  override def createRecordReader(
-    split: InputSplit,
-    context: TaskAttemptContext): RecordReader[Text, Text] = {
-    val reader =
-      new ConfigurableCombineFileRecordReader(split, context, classOf[TFileRecordReader])
-    reader.setConf(getConf)
-    reader
-  }
-
-  /**
-   * Allow minPartitions set by end-user in order to keep compatibility with old Hadoop API,
-   * which is set through setMaxSplitSize
-   */
-  def setMinPartitions(context: JobContext, minPartitions: Int) {
-    val files = listStatus(context).asScala
-    val totalLen = files.map(file => if (file.isDirectory) 0L else file.getLen).sum
-    val maxSplitSize = Math.ceil(totalLen * 1.0 /
-      (if (minPartitions == 0) 1 else minPartitions)).toLong
-    super.setMaxSplitSize(maxSplitSize)
-  }
-}
-
-class ConfigurableCombineFileRecordReader[K, V](
-  split: InputSplit,
-  context: TaskAttemptContext,
-  recordReaderClass: Class[_ <: RecordReader[K, V] with Configurable])
-  extends CombineFileRecordReader[K, V](
-    split.asInstanceOf[CombineFileSplit],
-    context,
-    recordReaderClass
-  ) with Configurable {
-
-  override def initNextRecordReader(): Boolean = {
-    val r = super.initNextRecordReader()
-    if (r) {
-      this.curReader.asInstanceOf[Configurable].setConf(getConf)
-    }
-    r
-  }
-}
-
-trait Configurable extends TConfigurable {
-   private var conf: Configuration = _
-   def setConf(c: Configuration) {
-      conf = c
-   }
-   def getConf: Configuration = conf
+  override def isSplitable(context: JobContext, filename: Path): Boolean = false
 }
--- src/main/scala/org/apache/spark/TFileRDD.scala
+++ src/main/scala/org/apache/spark/TFileRDD.scala
@@ -18,58 +18,13 @@
 package org.apache.spark
 
 import com.qubole.parser.TFileInputFormat
-import org.apache.hadoop.conf.{Configurable, Configuration}
-import org.apache.hadoop.io.{Text, Writable}
-import org.apache.hadoop.mapreduce._
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat
-import org.apache.hadoop.mapreduce.task.JobContextImpl
-import org.apache.spark.rdd.{NewHadoopPartition, NewHadoopRDD, RDD}
-
-class TFileRDD(sc: SparkContext,
-    inputFormatClass: Class[_ <: TFileInputFormat],
-    keyClass: Class[Text],
-    valueClass: Class[Text],
-    conf: Configuration,
-    minPartitions: Int)
-  extends NewHadoopRDD[Text, Text](sc, inputFormatClass, keyClass, valueClass, conf) {
-
-  override def getPartitions: Array[Partition] = {
-    val conf = getConf
-    // setMinPartitions below will call FileInputFormat.listStatus(), which can be quite slow when
-    // traversing a large number of directories and files. Parallelize it.
-    conf.setIfUnset(FileInputFormat.LIST_STATUS_NUM_THREADS,
-      Runtime.getRuntime.availableProcessors().toString)
-    val inputFormat = inputFormatClass.newInstance
-    inputFormat match {
-      case configurable: Configurable =>
-        configurable.setConf(conf)
-      case _ =>
-    }
-
-    val jobContext = new JobContextImpl(conf, jobId)
-    inputFormat.setMinPartitions(jobContext, minPartitions)
-    val rawSplits = inputFormat.getSplits(jobContext).toArray
-    val result = new Array[Partition](rawSplits.size)
-    for (i <- 0 until rawSplits.size) {
-      result(i) = new NewHadoopPartition(id, i, rawSplits(i).asInstanceOf[InputSplit with Writable])
-    }
-    result
-  }
-}
+import org.apache.hadoop.io.Text
+import org.apache.spark.rdd.RDD
 
 object TFileRDD {
-  def apply(sc: SparkContext,
-      path: String,
-      minPartitions: Int = 2): RDD[(String, String)] = {
-    val job = Job.getInstance(sc.hadoopConfiguration)
-    FileInputFormat.setInputPaths(job, path)
-    val updateConf = job.getConfiguration
-    new TFileRDD(
-      sc,
-      classOf[TFileInputFormat],
-      classOf[Text],
-      classOf[Text],
-      updateConf,
-      minPartitions).map(record => (record._1.toString, record._2.toString)).setName(path)
+  def apply(
+      sc: SparkContext,
+      path: String): RDD[(Text, Text)] = {
+    sc.newAPIHadoopFile(path, classOf[TFileInputFormat], classOf[Text], classOf[Text])
   }
 }
